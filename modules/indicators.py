98755027c3b16a7608b6c5118a6934026ae0038e
pmnick <nickmorrill3@gmail.com>
2017-08-17 08:35:45 -0600

    LightIndicator is now a frame containing optional label and light

--- modules/indicators.py
+++ modules/indicators.py
@@ -1,4 +1,4 @@
-from Tkinter import Label, Canvas, StringVar
+from Tkinter import Label, Canvas, StringVar, Frame,W
 from pdb import set_trace as debugger
 
 class TextIndicator(Label, object):
@@ -15,19 +15,29 @@ class TextIndicator(Label, object):
         self.value.set('{}: {}'.format(self.prefix, new_value))
 
 
-class LightIndicator(Canvas, object):
-    def __init__(self, parent, r=10, state='disabled'):
+class LightIndicator(Frame, object):
+    def __init__(self, parent, r=10, text='', state='disabled'):
         # valid_states = ['normal', 'disabled', 'hidden']
-        super(LightIndicator, self).__init__(parent, width=r * 2 + 2, height=r * 2 + 2)
+        super(LightIndicator, self).__init__(parent)
         self.parent = parent
-        self.r = r
+        # initialize label
+        if text:
+            self.value = StringVar()
+            self.value.set(text)
+            self.label = Label(self, textvariable=self.value, padx=5, font=("Helvietica", 14))
+            self.label.pack()
+            self.label.grid(row=0, column=0)
 
-        # create indicator
-        self.light = self.create_oval(3, 3, r * 2 + 3, r * 2 + 3,\
+        # initialize indicator
+        self.canvas = Canvas(self, width=r * 2 + 2, height=r * 2 + 2)
+        self.canvas.pack()
+        self.canvas.grid(row=0, column=1)
+        self.r = r
+        self.light = self.canvas.create_oval(3, 3, r * 2 + 3, r * 2 + 3,\
             fill="green", disabledfill="red", state=state)
 
     def set_state(self, turn_on):
         if turn_on:
-            self.itemconfig(self.light, state='normal')
+            self.canvas.itemconfig(self.light, state='normal')
         else:
-            self.itemconfig(self.light, state='disable')
+            self.canvas.itemconfig(self.light, state='disable')
